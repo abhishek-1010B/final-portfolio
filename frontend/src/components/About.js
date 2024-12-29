dbafe8a2073e757ec6eb79f32b3ed05f87f952ca
abhishek-1010B <abhishek1010.be21@chitkarauniversity.edu>
2024-12-29 21:12:31 +0530

    Profile section updated

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -4,14 +4,14 @@ import "../styles/styles.css";
 const About = () => {
   return (
     <div className="container">
-      <h1 className="about-text">
-        I am Abhishek Duggal, <br /> A passionate freelancer, full-stack
-        developer.
-      </h1>
-      <div>
-        <p>12th Board: Ch. Balbir Public School</p>
-        <p>10th Board: Mount Carmel School (ICSE)</p>
-      </div>
+      {/* <h1 className="about-text"> */}
+        {/* I am Abhishek Duggal, <br /> A passionate freelancer, full-stack */}
+        {/* developer. */}
+      {/* </h1> */}
+      {/* <div> */}
+        {/* <p>12th Board: Ch. Balbir Public School</p> */}
+        {/* <p>10th Board: Mount Carmel School (ICSE)</p> */}
+      {/* </div> */}
     </div>
   );
 };
--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,13 +1,19 @@
 import React from "react";
 import "../styles/styles.css";
-import Mypic from "./assests/My pic.jpeg"
+import Typewriter from "typewriter-effect"; 
+import Mypic from "./assests/Abhi.JPG";
+
 const Home = () => {
   return (
     <div className="container">
+      {/* Left side: Profile Image */}
       <div className="left-side">
-        <img src={Mypic} alt="Abhishek Duggal" />
+        <img src={Mypic} alt="Abhishek Duggal" className="profile-pic" />
       </div>
+
+      {/* Right side: Navbar and Typing Effect */}
       <div className="right-side">
+        {/* Navigation Bar */}
         <nav className="navbar">
           <a href="#home">Home</a>
           <a href="#about">About</a>
@@ -15,6 +21,26 @@ const Home = () => {
           <a href="#projects">Projects</a>
           <a href="#contact">Contact</a>
         </nav>
+
+        {/* Typing Effect */}
+        <div className="intro-text">
+          <h1 className="highlight">
+            <Typewriter
+              options={{
+                strings: [
+                  "I am Abhishek Duggal",
+                  "A passionate freelancer",
+                  "Full Stack Developer",
+                ],
+                autoStart: true,
+                loop: true,
+                delay: 75,
+                deleteSpeed: 50,
+                pauseFor: 1500,
+              }}
+            />
+          </h1>
+        </div>
       </div>
     </div>
   );
