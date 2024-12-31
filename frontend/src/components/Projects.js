711d8228287977296c475a46091c4858000438dd
abhishek-1010B <abhishek1010.be21@chitkarauniversity.edu>
2024-12-31 16:11:47 +0530

    Projects Section Added

--- frontend/src/components/Projects.js
+++ frontend/src/components/Projects.js
@@ -1,13 +1,22 @@
 import React from "react";
-import "../styles/styles.css";
+import "../styles/projects.css";
 
 const Projects = () => {
   return (
-    <div className="container">
-      <div className="card">E-commerce (GitHub Link)</div>
-      <div className="card">Carbon Footprint (GitHub Link)</div>
-      <div className="card">Tourism Website (Go-Adventure)</div>
-      <div className="card">Currently working on (New Concept Design)</div>
+    <div id="projects">
+      <h1>Projects</h1>
+      <div className="project-box">Ecommerce Website</div>
+      <div className="project-box">Tourism Website</div>
+      <div className="project-box">Flipkart Clone</div>
+      <div className="project-box">Chat Bot</div>
+      <div className="project-box">Carbon Footprint</div>
+      <div className="project-box">Under Maintenance</div>
+
+      {/* Bubbles */}
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
+      <div className="bubble"></div>
     </div>
   );
 };
